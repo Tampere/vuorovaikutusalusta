f624d53b3b91064816bf273efc56a253f7bfad48
jlaamanen <joona.laamanen@gmail.com>
2022-02-14 13:25:44 +0200

    Merge pull request #116 from ubigu/bugfix/page-and-section-deletion-fixes
    Implemented drag and drop for pages and sections

--- client/src/components/admin/SurveySections.tsx
+++ client/src/components/admin/SurveySections.tsx
@@ -1,6 +1,7 @@
 import { SurveyPageSection } from '@interfaces/survey';
 import { useSurvey } from '@src/stores/SurveyContext';
 import React from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import SurveySectionAccordion from './SurveySectionAccordion';
 
 interface Props {
@@ -12,28 +13,61 @@ interface Props {
 }
 
 export default function SurveySections(props: Props) {
-  const { editSection, deleteSection } = useSurvey();
+  const { editSection, deleteSection, moveSection } = useSurvey();
+
   return (
-    <div>
-      {props.sections.map((section, index) => (
-        <SurveySectionAccordion
-          // Cannot rely on section.id, because it's null when the section doesn't exist in DB.
-          key={`${props.pageId}/${index}`}
-          disabled={props.disabled}
-          section={section}
-          name={`section-${index}`}
-          expanded={props.expandedSection === index}
-          onExpandedChange={(isExpanded) => {
-            props.onExpandedSectionChange(isExpanded ? index : null);
-          }}
-          onEdit={(section) => {
-            editSection(props.pageId, index, section);
-          }}
-          onDelete={() => {
-            deleteSection(props.pageId, index);
-          }}
-        />
-      ))}
-    </div>
+    <DragDropContext
+      onDragEnd={(event) => {
+        if (!event.destination) {
+          return;
+        }
+        const id = Number(event.draggableId);
+        const oldIndex = props.sections.findIndex(
+          (section) => section.id === id
+        );
+        const newIndex = event.destination.index;
+        moveSection(props.pageId, oldIndex, event.destination.index);
+        // If the section was expanded, re-expand with the new index
+        if (props.expandedSection === oldIndex) {
+          props.onExpandedSectionChange(newIndex);
+        }
+      }}
+    >
+      <Droppable droppableId="sections">
+        {(provided, _snapshot) => (
+          <div {...provided.droppableProps} ref={provided.innerRef}>
+            {props.sections.map((section, index) => (
+              <Draggable
+                key={section.id}
+                draggableId={String(section.id)}
+                index={index}
+              >
+                {(provided, _snapshot) => (
+                  <SurveySectionAccordion
+                    provided={provided}
+                    disabled={props.disabled}
+                    section={section}
+                    name={`section-${index}`}
+                    expanded={props.expandedSection === index}
+                    onExpandedChange={(isExpanded) => {
+                      props.onExpandedSectionChange(isExpanded ? index : null);
+                    }}
+                    onEdit={(section) => {
+                      editSection(props.pageId, index, section);
+                    }}
+                    onDelete={() => {
+                      deleteSection(props.pageId, index);
+                      // Reset expanded section to null
+                      props.onExpandedSectionChange(null);
+                    }}
+                  />
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
