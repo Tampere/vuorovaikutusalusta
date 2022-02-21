9eb094075b37799f51b9432bc50a8f1fdf2385ee
Joona Laamanen <joona.laamanen@gmail.com>
2022-02-21 16:14:26 +0200

    Fixed line breaks in markdown contents

--- client/src/components/TextSection.tsx
+++ client/src/components/TextSection.tsx
@@ -2,6 +2,7 @@ import { SurveyTextSection } from '@interfaces/survey';
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import rehypeExternalLinks from 'rehype-external-links';
+import remarkBreaks from 'remark-breaks';
 
 interface Props {
   section: SurveyTextSection;
@@ -11,7 +12,10 @@ export default function TextSection({ section }: Props) {
   return (
     <>
       {section.title && <h2>{section.title}</h2>}
-      <ReactMarkdown rehypePlugins={[rehypeExternalLinks]}>
+      <ReactMarkdown
+        rehypePlugins={[rehypeExternalLinks]}
+        remarkPlugins={[remarkBreaks]}
+      >
         {section.body}
       </ReactMarkdown>
     </>
