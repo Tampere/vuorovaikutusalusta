292464e16ec74021392d6884e98de152ed08af03
Ossi Tammi <ossi.tammi@gmail.com>
2022-04-05 13:52:12 +0300

    Merge pull request #153 from ubigu/feature/page-links
    Page linkin design fixes

---
+++ client/src/components/PageConnector.tsx
@@ -0,0 +1,30 @@
+import { Theme } from '@material-ui/core';
+import React from 'react';
+
+interface PageConnectorProps {
+  activePage: number;
+  pageIndex: number;
+  theme: Theme;
+}
+
+export default function PageConnector({
+  activePage,
+  pageIndex,
+  theme,
+}: PageConnectorProps) {
+  return (
+    <div style={{ marginLeft: '12px', flex: '1 1 auto' }}>
+      <span
+        style={{
+          display: 'block',
+          borderLeft: `${
+            activePage === pageIndex
+              ? `3px solid ${theme.palette.primary.main}`
+              : '2px solid grey'
+          }`,
+          minHeight: '24px',
+        }}
+      ></span>
+    </div>
+  );
+}
