f9b502b7855a1fe557d3ea8a3968f6ae761c7d1d
Joona Laamanen <joona.laamanen@gmail.com>
2022-05-19 15:08:18 +0300

    Fixed migration file

--- server/migrations/1652181277733_unfinished-submission.ts
+++ server/migrations/1652181277733_unfinished-submission.ts
@@ -4,7 +4,6 @@ export const shorthands: ColumnDefinitions | undefined = undefined;
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
   pgm.sql(`
-    CREATE EXTENSION IF NOT EXISTS pgcrypto;
     ALTER TABLE data.submission ADD COLUMN unfinished_token UUID;
   `);
 }
@@ -12,6 +11,5 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
 export async function down(pgm: MigrationBuilder): Promise<void> {
   pgm.sql(`
     ALTER TABLE data.submission DROP COLUMN unfinished_token;
-    DROP EXTENSION pgcrypto;
   `);
 }
