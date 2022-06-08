401147f03f1d55243771fb0b906afddf5bddc697
Joona Laamanen <joona.laamanen@gmail.com>
2022-06-08 14:31:12 +0300

    Updated landing page theming

--- client/src/components/SurveyLandingPage.tsx
+++ client/src/components/SurveyLandingPage.tsx
@@ -7,7 +7,7 @@ import React from 'react';
 import TreBanner from './logos/TreBanner';
 import TreLogo from './logos/TreLogo';
 
-const useStyles = makeStyles((theme: Theme) => ({
+const useStyles = makeStyles((theme: Theme & { [customKey: string]: any }) => ({
   root: (props: any) => ({
     display: 'flex',
     width: '100%',
@@ -21,6 +21,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     backgroundAttachment: 'fixed',
+    backgroundColor: theme.landingPage?.backgroundColor ?? '#fff',
   }),
   header: {
     display: 'flex',
@@ -34,7 +35,6 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   heading: {
     textTransform: 'uppercase',
-    fontFamily: '"Montserrat", sans-serif',
     fontSize: '2rem',
     wordBreak: 'break-word',
     hyphens: 'auto',
@@ -47,36 +47,40 @@ const useStyles = makeStyles((theme: Theme) => ({
       color: theme.palette.primary.contrastText,
       padding: '1rem',
       boxDecorationBreak: 'clone',
+      ...theme.landingPage?.title,
       [theme.breakpoints.down(600)]: {
         padding: '2vw',
       },
     },
   },
   title: {
+    fontFamily: '"Montserrat", sans-serif',
     fontSize: '3rem',
+    ...theme.landingPage?.title,
     [theme.breakpoints.down(600)]: {
       fontSize: '9vw',
     },
   },
   subtitle: {
+    fontFamily: '"Montserrat", sans-serif',
     fontSize: '2rem',
+    ...theme.landingPage?.subtitle,
     [theme.breakpoints.down(600)]: {
       fontSize: '6vw',
     },
-    textTransform: 'none',
   },
   start: {
     fontFamily: '"Montserrat", sans-serif',
-    fontWeight: 600,
+    fontSize: '2rem',
     backgroundColor: theme.palette.secondary.main,
     color: theme.palette.secondary.contrastText,
-    fontSize: '2rem',
     padding: '0.5rem',
     textDecoration: 'none',
     marginTop: '1.5rem',
     '&:hover': {
       // TODO: hover effect for start button?
     },
+    ...theme.landingPage?.start,
     [theme.breakpoints.down(600)]: {
       fontSize: '6vw',
     },
