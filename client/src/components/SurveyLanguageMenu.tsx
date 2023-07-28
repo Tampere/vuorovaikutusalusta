8b614ac574e6b8715ed937783013adccf4cb26c0
Janne Peltonen <5507205+jpeltzi@users.noreply.github.com>
2023-07-28 14:59:37 +0300

    Re-implement language menu accessibly
    The language menu is now implemented with off-the-shelf <Select> element. The previous implementation was not keyboard nor - by extension - screen reader accessible.

--- client/src/components/SurveyLanguageMenu.tsx
+++ client/src/components/SurveyLanguageMenu.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { Menu, MenuItem, Tooltip, Typography } from '@mui/material';
+import { MenuItem, Select } from '@mui/material';
 import { useTranslations } from '@src/stores/TranslationContext';
 import { LanguageCode } from '@interfaces/survey';
 import { makeStyles } from '@mui/styles';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import LanguageIcon from '@mui/icons-material/Language';
+import React from 'react';
 
 interface Props {
   style?: React.CSSProperties;
@@ -25,65 +25,37 @@ export default function SurveyLanguageMenu({
 }: Props) {
   const { tr, setSurveyLanguage, setLanguage, languages, surveyLanguage } =
     useTranslations();
-  const [anchorEl, setAnchorEl] = useState(null);
   const classes = useStyles();
-  const open = Boolean(anchorEl);
-
-  const handleClick = (event: any) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
 
   return (
     <div className={classes.root} style={style}>
-      <Tooltip
-        id="tooltip-button"
-        title={tr.SurveyLanguageMenu.changeSurveyLanguage}
-      >
-        <div
-          onClick={(event) => handleClick(event)}
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-          }}
-        >
-          <Typography>
-            {tr.SurveyLanguageMenu.surveyLanguage} (
-            {surveyLanguage?.toLocaleUpperCase()})
-          </Typography>
-          <ArrowDropDownIcon />
-        </div>
-      </Tooltip>
-      <Menu
-        style={{ display: !anchorEl ? 'none' : '' }}
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'tooltip-button',
+      <Select
+        inputProps={{"aria-label": tr.SurveyLanguageMenu.languageControl}}
+        value={surveyLanguage}
+        onChange={(event) => {
+          const targetLanguage = event.target.value as LanguageCode;
+          setSurveyLanguage(targetLanguage);
+          if (changeUILanguage) setLanguage(targetLanguage);
+        }}
+        IconComponent={LanguageIcon}
+        sx={{
+          /* Visual label not used, hide the border and legend */
+          '& fieldset': {
+            borderWidth: 0,
+            '&>legend': {display: 'none'}
+          }
         }}
       >
         {languages.map((lang, index) => (
           <MenuItem
             key={`lang-item-${index}`}
-            lang={lang}
+            value={lang}
             selected={lang === surveyLanguage}
-            onClick={(event) => {
-              handleClose();
-              const targetLanguage = (event.target as HTMLInputElement)
-                .lang as LanguageCode;
-              setSurveyLanguage(targetLanguage);
-              if (changeUILanguage) setLanguage(targetLanguage);
-            }}
           >
             {tr.LanguageMenu[lang]} ({lang.toLocaleUpperCase()})
           </MenuItem>
         ))}
-      </Menu>
+      </Select>
     </div>
   );
 }
