81ac032362b9edbed34aaa4235909b8cc1990e31
Janne Peltonen <5507205+jpeltzi@users.noreply.github.com>
2023-08-01 10:14:12 +0300

    Ordering question drag and drop (#140)
    * Implement layout and styling
    * Attach DnD structure to the cards
    * Complete DnD action
    * Style improvements
    * Manage SR announcements
    * Announce item position on focus
    * Implement state management
    * Modify form validation to accommodate DnD
    * Apply state feedback styling
    * Clean up code based on PR comments
    * Clean up code further

--- client/src/components/SortingQuestion.tsx
+++ client/src/components/SortingQuestion.tsx
@@ -1,15 +1,22 @@
 import { SurveySortingQuestion } from '@interfaces/survey';
 import {
-  FormControl,
+  Box,
+  Checkbox,
   FormControlLabel,
   FormGroup,
-  MenuItem,
   Paper,
-  Select,
+  Typography,
 } from '@mui/material';
-import { makeStyles } from '@mui/styles';
+import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
+import { DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+  ResponderProvided
+} from 'react-beautiful-dnd';
 import { useTranslations } from '@src/stores/TranslationContext';
 import React, { useEffect, useState } from 'react';
+import { visuallyHidden } from '@mui/utils';
 
 interface Props {
   value: number[];
@@ -18,122 +25,137 @@ interface Props {
   setDirty: (dirty: boolean) => void;
 }
 
-const useStyles = makeStyles({
-  label: {
-    marginLeft: '1rem',
-  },
-  select: {
-    minWidth: '4rem',
-  },
-  fieldRow: {
-    margin: '0.5rem',
-    marginLeft: '1rem',
-  },
-  finalListPaper: {
-    display: 'inline-block',
-    padding: '1rem',
-    paddingLeft: 0,
-    margin: '1rem',
-  },
-});
-
 export default function SortingQuestion(props: Props) {
-  const { tr, surveyLanguage } = useTranslations();
+  const { surveyLanguage, tr } = useTranslations();
+  const [verified, setVerified] = useState(props.value ? true : false);
   const [sortedOptionIds, setSortedOptionIds] = useState(
-    props.value ?? new Array(props.question.options.length).fill(null)
+    props.value ?? props.question.options.map( option => option.id )
   );
 
-  const classes = useStyles();
-
   useEffect(() => {
-    if (!sortedOptionIds || !props.onChange) {
+    if (!verified || !props.onChange) {
+      props.onChange(null);
       return;
     }
     props.onChange(sortedOptionIds);
-  }, [sortedOptionIds]);
+  }, [sortedOptionIds, verified]);
+
+  const reorder = (list: Array<number>, startIndex: number, endIndex: number) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+    return result;
+  };
+
+  const onDragEnd = (result: DropResult, provided: ResponderProvided) => {
+    const { destination, source } = result;
+    if (!destination || destination.index === source.index) {
+      provided.announce(tr.SortingQuestion.announcement.droppedInPlace
+        .replace('{position}', (destination.index + 1).toString()));
+      return;
+    }
+    provided.announce(tr.SortingQuestion.announcement.drop
+      .replace('{position}', (destination.index + 1).toString())
+    );
+    setVerified(false);
+    setSortedOptionIds(reorder(sortedOptionIds, source.index, destination.index));
+  };
 
   return (
-    <FormGroup
-      id={`${props.question.id}-input`}
-      style={{ display: 'flex', flexDirection: 'row' }}
-    >
-      <div style={{ flexGrow: 1 }}>
-        {props.question.options.map((option) => (
-          <FormGroup row key={option.id} className={classes.fieldRow}>
-            <FormControl>
-              <FormControlLabel
-                label={option.text?.[surveyLanguage] ?? ''}
-                labelPlacement="end"
-                classes={{ label: classes.label }}
-                control={
-                  <Select
-                    aria-label={tr.SortingQuestion.rankForOption.replace(
-                      '{option}',
-                      option.text?.[surveyLanguage]
-                    )}
-                    className={classes.select}
-                    value={
-                      sortedOptionIds.includes(option.id)
-                        ? sortedOptionIds.indexOf(option.id)
-                        : ''
-                    }
-                    onChange={(event) => {
-                      props.setDirty(true);
-                      // Remove possible previous value
-                      const newSortedOptionIds = sortedOptionIds.map(
-                        (optionId) => (optionId === option.id ? null : optionId)
-                      );
-                      if (event.target.value === '') {
-                        // If empty was selected, leave the option ID out of the array
-                        setSortedOptionIds(newSortedOptionIds);
-                        return;
-                      }
-                      setSortedOptionIds(
-                        newSortedOptionIds.map((optionId, index) =>
-                          // Only replace the array item with the requested index with the current option
-                          index === Number(event.target.value)
-                            ? option.id
-                            : optionId
-                        )
-                      );
-                    }}
-                  >
-                    <MenuItem value="">
-                      <em>{tr.SortingQuestion.empty}</em>
-                    </MenuItem>
-                    {new Array(props.question.options.length)
-                      .fill(null)
-                      .map((_, index) => (
-                        <MenuItem
-                          key={index}
-                          value={index}
-                          disabled={sortedOptionIds[index] != null}
-                        >
-                          {index + 1}
-                        </MenuItem>
-                      ))}
-                  </Select>
-                }
-              />
-            </FormControl>
-          </FormGroup>
-        ))}
-      </div>
-      <div style={{ flexGrow: 1 }}>
-        <Paper elevation={3} className={classes.finalListPaper}>
-          <ol aria-label={tr.SortingQuestion.sortedOptions}>
-            {sortedOptionIds.map((optionId, index) => (
-              <li key={index}>
-                {optionId == null
-                  ? '-'
-                  : props.question.options.find(
-                      (option) => option.id === optionId
-                    ).text?.[surveyLanguage]}
-              </li>
-            ))}
-          </ol>
-        </Paper>
-      </div>
+    <FormGroup id={`${props.question.id}-input`}>
+      <Box style={visuallyHidden} id={`drag-instruction-announcement-${props.question.id}`}>
+        {tr.SortingQuestion.announcement.focus}
+      </Box>
+      <DragDropContext
+        onDragStart={(start, provided) => provided.announce(
+          tr.SortingQuestion.announcement.grab
+          .replace('{position}', (start.source.index + 1).toString())
+        )}
+        onDragUpdate={(update, provided) => provided.announce(
+          tr.SortingQuestion.announcement.move
+          .replace('{position}', (update.destination.index + 1).toString())
+          .replace('{length}', props.question.options.length.toString())
+        )}
+        onDragEnd={onDragEnd}>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <div>
+          {props.question.options.map((_option, index) => (
+            <Paper key={index}
+              variant="outlined"
+              sx={{
+                backgroundColor: "#ededed",
+                borderTopRightRadius: "0",
+                borderBottomRightRadius: "0",
+                marginBottom: "0.5em",
+                marginRight: "-0.25em",
+                padding: "0.5em",
+              }}
+            >
+              <Typography>
+                {index+1}.
+              </Typography>
+            </Paper>
+          ))}
+          </div>
+          <Droppable droppableId={`question-dropzone-${props.question.id}`}>
+            {(provided) => (
+              <div {...provided.droppableProps} ref={provided.innerRef} style={{ flexGrow: 1 }}>
+              {sortedOptionIds.map((optionId, index) => (
+                <Draggable
+                  key={`option-${optionId}`}
+                  index={index}
+                  draggableId={`option-${optionId}`}>
+                  {(provided, snapshot) => (
+                    <Paper
+                      variant="outlined"
+                      sx={{
+                        backgroundColor: snapshot.isDragging ? "#c2dcf1" : "white",
+                        display: "flex",
+                        justifyContent: "space-between",
+                        marginBottom: "0.5em",
+                        padding: "0.5em",
+                        transition: "background-color 200ms",}}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      ref={provided.innerRef}
+                      aria-describedby={`drag-instruction-announcement-${props.question.id}`}
+                    >
+                      <Typography color="primary">
+                        { props.question.options
+                          .find( option => option.id === optionId )
+                          .text?.[surveyLanguage] }
+                      </Typography>
+                      <Box style={visuallyHidden}>
+                        {tr.SortingQuestion.inPosition} {index + 1} / {sortedOptionIds.length}
+                        </Box>
+                      <DragIndicatorIcon color={snapshot.isDragging ? "info" : "action"} />
+                    </Paper>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </div>
+        <FormControlLabel
+          label={tr.SortingQuestion.orderComplete}
+          control={
+            <Checkbox
+              name={`verify-order-question_${props.question.id}`}
+              checked={verified}
+              onChange={(event) => {
+                setVerified(event.target.checked); 
+                props.setDirty(true);
+              }}
+            />
+          }
+          sx={{
+            lineHeight: 1.2,
+            marginBottom: '0.5em',
+          }}
+        />
+      </DragDropContext>
     </FormGroup>
   );
 }
