7b4cd9144846f785c0318b3e9f338c1a0ec2c08b
Mikael Moilanen <mikael.moilanen@gmail.com>
2023-12-19 15:37:38 +0200

    Fix latest migration

--- server/migrations/1702987902585_page-condition-constraint.ts
+++ server/migrations/1702987902585_page-condition-constraint.ts
@@ -3,10 +3,12 @@ import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate';
 export const shorthands: ColumnDefinitions | undefined = undefined;
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
-  pgm.sql(`
+  pgm.sql(`BEGIN;
+  ALTER TABLE data.section_conditions DROP CONSTRAINT section_conditions_survey_page_id_fkey;
   ALTER TABLE data.section_conditions
   ADD CONSTRAINT fk_survey_page_id
   FOREIGN KEY (survey_page_id)
   REFERENCES data.survey_page(id)
-  ON DELETE CASCADE;`);
+  ON DELETE CASCADE;
+  COMMIT;`);
 }
