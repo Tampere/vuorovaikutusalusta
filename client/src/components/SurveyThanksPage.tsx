3c878587a32fd08fcac449d30ce39cc684ea9cd5
Ossi Tammi <>
2024-02-13 10:27:39 +0200

    Added Oulu specific info texts and links

--- client/src/components/SurveyThanksPage.tsx
+++ client/src/components/SurveyThanksPage.tsx
@@ -155,12 +155,7 @@ export default function SurveyThanksPage({ survey, isTestSurvey }: Props) {
             {tr.FooterLinks.accessibility}
           </Link>
           {survey.displayPrivacyStatement && (
-            <Link
-              color="primary"
-              underline="hover"
-              href="https://www.tampere.fi/tietosuoja-ja-tiedonhallinta/tietosuojaselosteet"
-              target="_blank"
-            >
+            <Link color="primary" underline="hover" href="" target="_blank">
               {tr.FooterLinks.privacyStatement}
             </Link>
           )}
