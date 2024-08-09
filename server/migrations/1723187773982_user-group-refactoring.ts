f1dbd3d89c193a005e8f498d6d4fafe9c90af22f
Toni <toni.kuikka@ubigu.fi>
2024-08-09 12:49:24 +0300

    Rename groups to organizations & add organization to the pk_files constraint in the db

---
+++ server/migrations/1723187773982_user-group-refactoring.ts
@@ -0,0 +1,26 @@
+import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate';
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.sql(
+    `
+      ALTER TABLE DATA.survey ALTER COLUMN groups TYPE TEXT;
+      ALTER TABLE DATA.survey RENAME groups TO organization;
+      ALTER TABLE APPLICATION.user RENAME groups TO organizations;
+      ALTER TABLE DATA.files ALTER COLUMN groups TYPE TEXT;
+      ALTER TABLE DATA.files RENAME groups TO organization;
+      ALTER TABLE DATA.files DROP CONSTRAINT pk_files CASCADE;
+      ALTER TABLE DATA.files ADD CONSTRAINT pk_files PRIMARY KEY (file_path, file_name, organization);
+      ALTER TABLE DATA.survey_page ADD sidebar_image_organization TEXT;
+      ALTER TABLE DATA.survey_page ADD CONSTRAINT fk_page_image FOREIGN KEY (sidebar_image_path, sidebar_image_name, sidebar_image_organization) REFERENCES data.files (file_path, file_name, organization) ON DELETE SET NULL;
+      ALTER TABLE DATA.page_section ADD file_organization TEXT;
+      ALTER TABLE DATA.page_section ADD CONSTRAINT fk_section_file FOREIGN KEY (file_path, file_name, file_organization) REFERENCES data.files (file_path, file_name, organization) ON DELETE SET NULL;
+      ALTER TABLE DATA.survey
+        ADD CONSTRAINT fk_background_image FOREIGN KEY (background_image_path, background_image_name, organization) REFERENCES data.files (file_path, file_name, organization) ON DELETE SET NULL,
+        ADD CONSTRAINT fk_thanks_page_image FOREIGN KEY (thanks_page_image_path, thanks_page_image_name, organization) REFERENCES DATA.files(file_path, file_name, organization) ON DELETE SET NULL,
+        ADD CONSTRAINT fk_top_margin_image FOREIGN KEY (top_margin_image_path, top_margin_image_name, organization) REFERENCES DATA.files(file_path, file_name, organization) ON DELETE SET NULL,
+        ADD CONSTRAINT fk_bottom_margin_image FOREIGN KEY (bottom_margin_image_path, bottom_margin_image_name, organization) REFERENCES DATA.files(file_path, file_name, organization) ON DELETE SET NULL;
+    `,
+  );
+}
