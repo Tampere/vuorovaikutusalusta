668c2724380150d3e92ca9611279c87093037802
Toni <toni.kuikka@ubigu.fi>
2024-08-21 15:56:52 +0300

    Revert "Lazy load images using IntersectionObserver"
    This reverts commit c8e73e66cf9b52092bb70f09d2b9821589136c72.

--- client/src/components/admin/SurveyImageListItem.tsx
+++ client/src/components/admin/SurveyImageListItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState, useRef, useEffect } from 'react';
+import { useState } from 'react';
 import { File } from '@interfaces/survey';
 import { ImageListItem, CircularProgress } from '@mui/material';
 import CancelIcon from '@src/components/icons/CancelIcon';
@@ -7,31 +7,19 @@ import { getFileName } from '@src/utils/path';
 
 interface Props {
   image: File;
-  src: string;
   altText: string;
+  limitToSvg?: boolean;
   onClick: Function;
   onDelete: Function;
 }
 
 export default function SurveyImageListItem(props: Props) {
-  const [status, setStatus] = useState<'notVisible' | 'loading' | 'visible'>(
-    'notVisible',
-  );
+  const [isLoading, setIsLoading] = useState(true);
   const image = props.image;
-
-  const imgContainer = useRef(null);
-  useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting) {
-        setStatus('loading');
-        observer.disconnect();
-      }
-    });
-    if (imgContainer?.current) observer.observe(imgContainer.current);
-  }, []);
+  const imageSrc = `data:image/${props.limitToSvg ? 'svg+xml' : ''};base64,${image.data}`;
 
   return (
-    <ImageListItem ref={imgContainer}>
+    <ImageListItem>
       <CancelIcon
         color="error"
         style={{
@@ -44,33 +32,28 @@ export default function SurveyImageListItem(props: Props) {
           await props.onDelete(event, image.fileUrl)
         }
       />
-      {status !== 'notVisible' && (
-        <img
-          src={props.src}
-          srcSet={props.src}
-          alt={props.altText || `survey-image-${getFileName(image.fileUrl)}`}
-          loading="lazy"
-          style={{
-            height: '100%',
-            objectFit: 'contain',
-            visibility: status === 'loading' ? 'hidden' : 'visible',
-            cursor: 'pointer',
-          }}
-          onLoad={() => {
-            setStatus('done');
-          }}
-          onError={() => {
-            setStatus('done');
-          }}
-          onClick={() => props.onClick(image.fileUrl)}
-        />
-      )}
+      <img
+        src={imageSrc}
+        srcSet={imageSrc}
+        alt={props.altText || `survey-image-${getFileName(image.fileUrl)}`}
+        loading="lazy"
+        style={{
+          height: '100%',
+          objectFit: 'contain',
+          visibility: isLoading ? 'hidden' : 'visible',
+          cursor: 'pointer',
+        }}
+        onLoad={() => {
+          setIsLoading(false);
+        }}
+        onClick={() => props.onClick(image.fileUrl)}
+      />
       <span
         style={{
           position: 'absolute',
           height: '100%',
           width: '100%',
-          display: status === 'loading' ? 'flex' : 'none',
+          display: isLoading ? 'flex' : 'none',
           alignItems: 'center',
           justifyContent: 'center',
         }}
