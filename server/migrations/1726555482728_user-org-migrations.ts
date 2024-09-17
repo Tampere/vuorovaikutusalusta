96c5f6e1181b2544176a86e9bc0bee444950802c
Ossi Tammi <>
2024-09-17 11:29:44 +0300

    Remove unnecessary characters from the user data

---
+++ server/migrations/1726555482728_user-org-migrations.ts
@@ -0,0 +1,14 @@
+/* eslint-disable no-useless-escape */
+import { ColumnDefinitions, MigrationBuilder } from 'node-pg-migrate';
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.sql(`
+    UPDATE application.user appuser
+    SET organizations = ARRAY(
+        SELECT REGEXP_REPLACE(unnest(appuser.organizations), '\[\"|\"\]', '', 'g')
+    )
+    WHERE appuser.id is not null;  
+  `)
+}
