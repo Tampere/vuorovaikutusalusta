e96798c73fe31ebc9f6a1f051919007fff10ed20
Toni <toni.kuikka@ubigu.fi>
2025-01-13 15:30:03 +0200

    Encrypt existing data in the migration

--- server/migrations/1736504593192_encrypt-users.ts
+++ server/migrations/1736504593192_encrypt-users.ts
@@ -1,6 +1,8 @@
 import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate';
+import { encryptionKey } from './../src/database';
 
 export const shorthands: ColumnDefinitions | undefined = undefined;
+
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
   pgm.sql(`
@@ -8,11 +10,11 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
   `);
   pgm.sql(
     `ALTER TABLE application.user
-      ALTER COLUMN full_name TYPE bytea USING full_name::bytea,
-      ALTER COLUMN email type bytea USING email::bytea,
+      ALTER COLUMN full_name TYPE bytea USING pgp_sym_encrypt(full_name, '${encryptionKey}')::bytea,
+      ALTER COLUMN email type bytea USING pgp_sym_encrypt(email, '${encryptionKey}')::bytea,
       ADD CONSTRAINT user_full_name_key UNIQUE (full_name),
       ADD CONSTRAINT user_email_key UNIQUE (email);
-    `,
+    `
   );
 }
 
@@ -24,8 +26,8 @@ export async function down(pgm: MigrationBuilder): Promise<void> {
     `ALTER TABLE application.user
       DROP CONSTRAINT IF EXISTS user_full_name_key,
       DROP CONSTRAINT IF EXISTS user_email_key,
-      ALTER COLUMN full_name TYPE text USING full_name::text,
-      ALTER COLUMN email TYPE text USING email::text;
-    `,
+      ALTER COLUMN full_name TYPE text USING pgp_sym_decrypt(full_name, '${encryptionKey}')::text,
+      ALTER COLUMN email TYPE text USING pgp_sym_decrypt(email, '${encryptionKey}')::text;
+    `
   );
 }
