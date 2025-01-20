6d4bb19689706a058ff759a4a13b8259c7d9f8ef
Toni <toni.kuikka@ubigu.fi>
2025-01-20 15:57:40 +0200

    Add included materials as columns to the publications table

--- server/migrations/1736241832049_add-publications-table.ts
+++ server/migrations/1736241832049_add-publications-table.ts
@@ -12,6 +12,10 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
       survey_id INT NOT NULL UNIQUE, 
       username TEXT NOT NULL,
       password TEXT NOT NULL UNIQUE,
+      alphanumeric_included BOOLEAN DEFAULT TRUE,
+      map_included BOOLEAN DEFAULT TRUE,
+      attachments_included BOOLEAN DEFAULT TRUE,
+      personal_included BOOLEAN DEFAULT TRUE,
       CONSTRAINT fk_survey
         FOREIGN KEY (survey_id)
         REFERENCES data.survey(id)
