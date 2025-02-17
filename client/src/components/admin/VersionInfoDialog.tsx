1e9eeaa60922ccd2d6ed58e224f819d093e6ae82
Mikael Moilanen <mikael.moilanen@gmail.com>
2025-02-17 11:08:40 +0200

    Add version info dialog

---
+++ client/src/components/admin/VersionInfoDialog.tsx
@@ -0,0 +1,123 @@
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  SxProps,
+  Typography,
+} from '@mui/material';
+import React from 'react';
+import CloseIcon from '../icons/CloseIcon';
+import KartallaLogo from '../icons/KartallaLogo';
+
+import UbiguLogoWhite from '../icons/UbiguLogoWhite';
+import { useTranslations } from '@src/stores/TranslationContext';
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const dialogBaseStyle: SxProps = {
+  '& .MuiPaper-root': {
+    backgroundColor: 'primary.main',
+    width: '404px',
+    height: 'fit-content',
+    border: '1px solid white',
+    borderRadius: '20px',
+    '& p': { color: 'white', fontSize: '14px' },
+  },
+};
+
+const dialogTitleStyle: SxProps = {
+  color: 'white',
+  marginTop: '30px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '1rem',
+};
+
+const dialogContentStyle: SxProps = {
+  color: 'white',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.5rem',
+  padding: '0px 24px',
+  flexGrow: 0,
+};
+
+const dialogActionsStyle: SxProps = {
+  color: 'white',
+  display: 'flex',
+  justifyContent: 'center',
+};
+
+export function VersionInfoDialog({ isOpen, onClose }: Props) {
+  const { tr } = useTranslations();
+  console.log('dialog');
+  return (
+    <Dialog open={isOpen} sx={dialogBaseStyle}>
+      <DialogTitle sx={dialogTitleStyle}>
+        <KartallaLogo />
+        <Box sx={{ textAlign: 'center' }}>
+          <Typography sx={{ fontWeight: 700 }}>
+            {tr.VersionInfoDialog.title}
+          </Typography>
+          <Typography>
+            {tr.VersionInfoDialog.version} {APP_VERSION}
+          </Typography>
+        </Box>
+      </DialogTitle>
+      <DialogContent sx={dialogContentStyle}>
+        <Typography>{tr.VersionInfoDialog.developedBy}:</Typography>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Box>
+            <Typography sx={{ fontWeight: 700, paddingBottom: '4px' }}>
+              Ubigu Oy
+            </Typography>
+            <Typography sx={{ lineHeight: '14px' }}>
+              Hämeenkatu 14 C 17, <br />
+              33100 Tampere
+            </Typography>
+            <Typography>ubigu.fi</Typography>
+          </Box>
+          <UbiguLogoWhite />
+        </Box>
+      </DialogContent>
+      <DialogActions sx={dialogActionsStyle}>
+        <Button
+          size="small"
+          variant="outlined"
+          sx={{
+            color: 'white',
+            '&:hover': {
+              backgroundColor: 'harmaa.main',
+              borderColor: 'white',
+            },
+            borderColor: 'white',
+            paddingX: '0.5rem',
+          }}
+          onClick={onClose}
+          endIcon={<CloseIcon htmlColor="white" />}
+        >
+          {tr.commands.close}
+        </Button>
+        <IconButton
+          sx={{ position: 'absolute', top: 0, right: 0 }}
+          onClick={onClose}
+        >
+          <CloseIcon htmlColor="white" />
+        </IconButton>
+      </DialogActions>
+    </Dialog>
+  );
+}
