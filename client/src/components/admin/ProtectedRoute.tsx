0a7db4b52f96c88a67334f6960931101a04dec83
Mikael Moilanen <mikael.moilanen@gmail.com>
2025-04-24 15:24:45 +0300

    Add initial setting for secure headers and logout page

--- client/src/components/admin/ProtectedRoute.tsx
+++ client/src/components/admin/ProtectedRoute.tsx
@@ -4,6 +4,10 @@ import React from 'react';
 import { PropsWithChildren } from 'react';
 import { Redirect, Route, RouteProps } from 'react-router-dom';
 
+/**
+ * A route that is only accessible to admin or super users.
+ * If the user is not an admin nor a super user, they are redirected to the login page.
+ */
 export function ProtectedRoute(props: PropsWithChildren<RouteProps>) {
   const { activeUserIsAdmin, activeUserIsSuperUser, isInitialized } = useUser();
 
