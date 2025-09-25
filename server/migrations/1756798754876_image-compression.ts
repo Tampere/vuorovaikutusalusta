85959cd5d78693ed256f0a87c84bec1bba0a89a5
Mikael Moilanen <mikael.moilanen@gmail.com>
2025-09-25 13:34:14 +0300

    Fix migrations

--- server/migrations/1756798754876_image-compression.ts
+++ server/migrations/1756798754876_image-compression.ts
@@ -4,30 +4,39 @@ import sharp from 'sharp';
 export const shorthands: ColumnDefinitions | undefined = undefined;
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
-  // First, add the column
-  await pgm.db.query(
-    `ALTER TABLE data.files ADD COLUMN compressed_file bytea;`,
-  );
+  await pgm.db.query('BEGIN');
 
-  const { rows } = await pgm.db.query(`
-            SELECT id, mime_type, file
-            FROM data.files
-            WHERE mime_type LIKE 'image/%';
-        `);
-
-  for (const row of rows) {
-    const fileBuffer = Buffer.from(row.file, 'base64');
-    const compressedFile = await sharp(fileBuffer)
-      .rotate()
-      .toFormat('jpeg', { quality: 20 })
-      .toBuffer();
+  try {
+    // First, add the column
     await pgm.db.query(
-      `
-        UPDATE data.files
-        SET compressed_file = $1
-        WHERE id = $2;
-      `,
-      [compressedFile, row.id],
+      `ALTER TABLE data.files ADD COLUMN compressed_file bytea;`,
     );
+
+    const { rows } = await pgm.db.query(`
+              SELECT id, mime_type, file
+              FROM data.files
+              WHERE mime_type LIKE 'image/%';
+          `);
+
+    for (const row of rows) {
+      const fileBuffer = Buffer.from(row.file, 'base64');
+      const compressedFile = await sharp(fileBuffer)
+        .rotate()
+        .toFormat('jpeg', { quality: 20 })
+        .toBuffer();
+      await pgm.db.query(
+        `
+          UPDATE data.files
+          SET compressed_file = $1
+          WHERE id = $2;
+        `,
+        [compressedFile, row.id],
+      );
+    }
+
+    await pgm.db.query('COMMIT');
+  } catch (error) {
+    await pgm.db.query('ROLLBACK');
+    throw error;
   }
 }
