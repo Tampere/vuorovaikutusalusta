c784a49d15a996b24f872cee524ebae36a01c075
Mikael Moilanen <mikael.moilanen@gmail.com>
2026-01-16 11:41:09 +0200

    Don't required authentication from /available-layers endpoint

--- server/src/routes/map.routes.ts
+++ server/src/routes/map.routes.ts
@@ -1,4 +1,3 @@
-import { ensureAuthenticated } from '@src/auth';
 import { validateRequest } from '@src/utils';
 import { Router } from 'express';
 import asyncHandler from 'express-async-handler';
@@ -9,13 +8,12 @@ const router = Router();
 
 router.get(
   '/available-layers',
-  ensureAuthenticated(),
   validateRequest([query('url').isString()]),
   asyncHandler(async (req, res) => {
     const mapUrl = decodeURIComponent(req.query.url.toString());
     const layers = await getAvailableMapLayers(mapUrl);
     res.json(layers);
-  })
+  }),
 );
 
 export default router;
